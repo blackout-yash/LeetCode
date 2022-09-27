241a7828bfc25f167dfa5f5279419c93ee165a06
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-09-27 19:56:47 +0530

    Time: 109 ms (21.14%), Space: 21.2 MB (34.84%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-	string pushDominoes(string dominoes) {
-		int prev = -1, n = dominoes.size();
-		for (int i = 0; i < n; i++) {
-			if (dominoes[i] == 'R') {
-				int j = i + 1;
-				while (true) {
-					if (j == n || dominoes[j] != '.') break;
-					j++;
-				} prev = j;
-                
-				if (j != n && dominoes[j] == 'L') {
-					int cnt = ((j - i) + 1) / 2, range = cnt, flag = 0;
-					if (((j - i) + 1) & 1) flag = 1;
-					for (int k = i; k <= j; k++) {
-						if (cnt) dominoes[k] = 'R', cnt--;
-						else dominoes[k] = 'L';
-					}
-					if (flag) dominoes[i + range] = '.';
-					i = j;
-				} else {
-					for (int k = i; k <= j; k++) dominoes[k] = 'R';
-					if (i + 1 == n) break;
-					else i = j - 1;
-				}
-			} else if(dominoes[i] == 'L') {
-				int j = i;
-				while (true) {
-					if (prev == j) break;
-					dominoes[j--] = 'L';
-				}
-			}
-		}
-		return dominoes;
-	}
+    string pushDominoes(string dominoes) {
+        int n = dominoes.size();
+        vector <int> left(n), right(n);
+        for(int i = n - 1; i >= 0; i--){
+            if(dominoes[i] == 'L') left[i] = 1;
+            else if(i + 1 != n && left[i + 1]) {
+                if(dominoes[i] == 'R') left[i] = 0;
+                else left[i] = 1 + left[i + 1];
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(dominoes[i] == 'R') right[i] = 1;
+            else if(i && right[i - 1]) {
+                if(dominoes[i] == 'L') right[i] = 0;
+                else right[i] = 1 + right[i - 1];
+            }
+        }
+        
+        string ans(n, '.');
+        for(int i = 0; i < n; i++){
+            if(left[i] && left[i] < right[i]) ans[i] = 'L';
+            else if(right[i] && left[i] > right[i]) ans[i] = 'R';
+            else if(left[i] > right[i]) ans[i] = 'L';
+            else if(left[i] < right[i]) ans[i] = 'R';
+        }
+        
+        return ans;
+    }
 };
