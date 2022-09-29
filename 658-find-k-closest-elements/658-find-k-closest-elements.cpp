2599c15e07075db99977bdf9b86bb2e2af0078da
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-09-29 19:24:30 +0530

    Time: 52 ms (88.36%), Space: 31.9 MB (61.64%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,17 +1,36 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector <pair <int, int>> temp;
-        for(auto y: arr) temp.push_back({abs(x - y), y});
-        sort(temp.begin(), temp.end());
+    vector<int> findClosestElements(vector<int> &arr, int k, int x) {
+        int n = arr.size(), low = 0, high = n - 1, start = high, diff = 1e9;
+        while(low <= high){
+            int mid = low + (high - low) / 2, num = arr[mid];
+            
+            if(num == x) {
+                start = mid;
+                break;
+            } else if(num > x) high = mid - 1;
+            else low = mid + 1;
+            
+            if(diff >= abs(num - x)) {                
+                if(diff == abs(num - x)) start = min(start, mid);
+                else start = mid;
+                diff = abs(num - x);
+            }
+        }  
+        
+        int i = start, j = start;
+        while(true){
+            if(j - i == k - 1) break;
+            else if(!i) j++;
+            else if(j + 1 == n) i--;
+            else {
+                if(abs(arr[i - 1] - x) <= abs(arr[j + 1] - x)) i--;
+                else j++;
+            }
+        }
         
         vector <int> ans;
-        for(auto y: temp){
-            if(k) {
-                ans.push_back(y.second);
-                k--;  
-            } else break;
-        } sort(ans.begin(), ans.end());
+        for(int k = i; k <= j; k++) ans.push_back(arr[k]);
         
         return ans;
     }
