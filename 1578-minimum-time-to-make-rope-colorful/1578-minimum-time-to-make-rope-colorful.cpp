e5148804b37ddc2975d817f4aa30515701e7cd4f
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-10-03 19:12:24 +0530

    Time: 171 ms (89.95%), Space: 95.5 MB (50.05%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-	int minCost(int i, char prev, string &colors, vector <int> &neededTime, vector <vector<int>> &dp) {
-		if (i == -1) return 0;
-		else if (dp[i][prev - 'a'] != -1) return dp[i][prev - 'a'];
-
-		int ans1 = 1e9;
-		if (prev != colors[i]) ans1 = minCost(i - 1, colors[i], colors, neededTime, dp);
-		int ans2 = neededTime[i] + minCost(i - 1, prev, colors, neededTime, dp);
-
-		return dp[i][prev - 'a'] = min(ans1, ans2);
-	}
-
-	int minCost(string colors, vector<int> &neededTime) {
-		int n = colors.size();
-		vector <vector<int>> dp(n, vector<int> (30, -1));
-
-		int ans = minCost(n - 1, '{', colors, neededTime, dp);
-
-		return ans;
-	}
+    int minCost(string colors, vector<int>& neededTime) {
+        int ans = 0, n = colors.size();
+        for(int i = 0; i < n; i++){
+            int j = i, sum = 0, _max = 0;
+            while(true){
+                if(j == n) break;
+                else if(colors[i] != colors[j]) break;
+                sum += neededTime[j];
+                _max = max(_max, neededTime[j]);
+                j++;
+            }
+            ans += (sum - _max);
+            i = j - 1;
+        }
+        return ans;
+    }
 };
