7c0a22dd174ae6284a276cff01d244468067104a
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-10-03 19:26:31 +0530

    Time: 1985 ms (5.08%), Space: 298.3 MB (5.02%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -6,12 +6,10 @@ public:
 		for (int i = 1; i <= n; i++) {
 			int ind = colors[i - 1] - 'a', _min = 1e9;
 			for (int j = 0; j < 26; j++) {
-				if (ind != j) {
-					_min = min(_min, dp[i - 1][j]);
-					dp[i][j] = dp[i - 1][j] + neededTime[i - 1];
-				}
+				if (ind != j) _min = min(_min, dp[i - 1][j]);
+				dp[i][j] = dp[i - 1][j] + neededTime[i - 1];
 			}
-			dp[i][ind] = min(_min, dp[i - 1][ind] + neededTime[i - 1]);
+			dp[i][ind] = min(_min, dp[i][ind]);
 		}
 		return *min_element(dp[n].begin(), dp[n].end());
 	}
