55a7626e61208f9a7c14b6eb40606b51b55e9747
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-10-06 22:28:09 +0530

    Time: 32 ms (57.99%), Space: 6.8 MB (60.03%) - LeetHub

--- 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
-	int pal[200][200], dp[200][200][200];
-
-	void preCompute(int n, int k, string &s) {
+	int pal[200][200];
+	void preCompute(int n, string &s) {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				int l = i, r = j, ans = 0;
@@ -12,33 +11,28 @@ public:
 				} pal[i][j] = ans;
 			}
 		}
-        
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                for(int ind = 0; ind < k; ind++) dp[i][j][ind] = -1;
-            }
-        }
-	}
-
-	int palindromePartition(int i, int j, int k, int n, string &s) {
-		if (j == n) {
-			if (k) return 1e9;
-			return pal[i][j - 1];
-		} else if(dp[i][j][k] != -1) return dp[i][j][k];
-
-		int ans1 = 1e9;
-		if (k && j + 1 != n) ans1 = pal[i][j] + palindromePartition(j + 1, j + 1, k - 1, n, s);
-		int ans2 = palindromePartition(i, j + 1, k, n, s);
-        
-		return dp[i][j][k] = min(ans1, ans2);
 	}
 
 	int palindromePartition(string s, int k) {
 		int n = s.size();
-		preCompute(n, k, s);
-        
-		int ans = palindromePartition(0, 0, k - 1, n, s);
+		preCompute(n, s);
 
-		return ans;
+		vector <vector<int>> dp(n, vector<int> (k, 1e9));
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < min(k, i + 1); j++) {
+                if(!j) {
+                    dp[i][j] = pal[0][i];
+                    continue;
+                }
+                
+				for (int ind = i; ind >= 1; ind--) {
+					int ans = pal[ind][i];
+				    if (ind) ans += dp[ind - 1][j - 1];
+				    dp[i][j] = min(dp[i][j], ans);
+				}
+			}
+		}
+        
+		return dp[n - 1][k - 1];
 	}
 };
