82777ae493a56a54be967f03b316cb4097fa8a4d
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-10-11 17:53:44 +0530

    Time: 89 ms (78.18%), Space: 61.6 MB (24.04%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
 	bool increasingTriplet(vector <int> &nums) {
-		long one = -1e10, two = one - 1;
+		int one = INT_MAX, two = INT_MAX;
 		for (auto x : nums) {
-			if (one <= -1e10) one = x;
-			else if (two <= -1e10) {
-				if (one < x) two = x;
-				else one = min(one, (long)x);
-			} else if (two > x) {
-				if (one < x) two = x;
-                else one = min(one, (long)x);
-			} else one = min(one, (long)x);
-            
-			if (one < two && two < x) return 1;
+			if(one >= x) one = x;
+            else if(two >= x) two = x;
+            else return 1;
 		}
 		return 0;
 	}
