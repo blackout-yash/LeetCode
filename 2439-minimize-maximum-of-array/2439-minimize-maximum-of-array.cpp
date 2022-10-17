7c2658c2f3014c1594990506283a0e2dea95de46
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-10-17 19:51:33 +0530

    Time: 606 ms (12.50%), Space: 71.3 MB (12.50%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,44 +1,25 @@
-// @author: aditya
+#define ll long long
 class Solution {
 public:
-    bool pos(vector<int>v,long long int x){
-        vector<long long>nums;
-        for(auto i:v){
-           nums.push_back((long long int )(i));
-        }
-        for(int i=nums.size()-1;i>=1;i--){
-            
-            if(nums[i]>=1&&nums[i]>x){
-                long long rem=nums[i]-x;
-                nums[i]=x;
-                nums[i-1]+=rem;
-            }
-            // break;
-        }
-        // for(auto i:nums){
-        //     cout<<i<<" ";
-        // }
-        // cout<<"\n";
-        if(*max_element(nums.begin(),nums.end())<=x){
-            return true;
-        }
-        return false;
-    }
-    int minimizeArrayValue(vector<int>& nums) {
-     
-        long long l=0,r=1e18;
-        long long  mid;
-        while(l<=r){
-            mid=l+(r-l)/2;
-            if(pos(nums,mid)){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return l;
-        // cout<<pos(nums,7);
-        // return mid;
-    }
+	bool isPossible(ll tar, vector <int> &nums) {
+		ll n = nums.size(), prev = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			if (nums[i] > tar) prev += (nums[i] - tar);
+			else prev -= min(prev, 1ll * (tar - nums[i]));
+		} 
+		if (prev) return 0;
+		return 1;
+	}
+
+	int minimizeArrayValue(vector<int> &nums) {
+		ll low = 0, high = 1e9 + 10, ans = high;
+		while (low <= high) {
+			ll mid = low + (high - low) / 2;
+			if (isPossible(mid, nums)) {
+				ans = mid;
+				high = mid - 1;
+			} else low = mid + 1;
+		}
+		return ans;
+	}
 };
