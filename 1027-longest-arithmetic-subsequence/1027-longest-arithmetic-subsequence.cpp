33519a4ec5da25ad7f1ebb9fc0b9c5833baf894b
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-12-10 19:08:46 +0530

    Time: 1183 ms (21.50%), Space: 279.9 MB (5.66%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -2,8 +2,16 @@ class Solution {
 public:
 	int longestArithSeqLength(vector <int> &nums) {
 		int n = nums.size(), ans = 0;
+
+		// here, i denotes the common difference
+		// since, 0 <= nums[i] <= 500, so maximum value of common differnce may be 500 or -500
 		for (int i = 0; i <= 500; i++) {
+
+			// 2d vector
+			// 1st row for -ve common differnce
+			// 2nd row for +ve common differnce
 			vector <vector<int>> dp(2, vector<int> (501));
+			
 			for (int j = 0; j < n; j++) {
 				int ind = nums[j];
 				if (i + ind > 500) dp[0][ind] = 1;
