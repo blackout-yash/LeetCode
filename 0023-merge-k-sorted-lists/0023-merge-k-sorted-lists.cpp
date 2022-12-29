6eb9ef2cb1cdf5ddbd005080bb30573bcd5dc9ef
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-12-29 12:58:45 +0530

    Time: 25 ms (89.87%), Space: 13.3 MB (58.55%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -1,25 +1,28 @@
-#define pp pair<int, ListNode*>
+class cmp {
+public:
+	bool operator()(ListNode* a, ListNode* b) {
+		if ((a -> val) > (b -> val)) return true;
+		else return false;
+	}
+};
+
 class Solution {
 public:
 	ListNode* mergeKLists(vector<ListNode*> &lists) {
-        if(lists.empty()) return NULL;
-        
 		ListNode* temp = new ListNode();
 		ListNode* head = temp;
-		priority_queue <pp, vector <pp>, greater <pp>> pq;
+		priority_queue <ListNode*, vector <ListNode*>, cmp> pq;
 		for (auto x : lists) {
-            if(x) pq.push({x -> val, x});
-        }
+			if (x) pq.push(x);
+		}
+
 		while (!pq.empty()) {
-			ListNode* dummy = pq.top().second;
+			ListNode* dummy = pq.top();
 			pq.pop();
 
 			temp -> next = dummy;
 			temp = temp -> next;
-			if (dummy -> next) {
-				dummy = dummy -> next;
-				pq.push({dummy -> val, dummy});
-			}
+			if (dummy -> next) pq.push(dummy -> next);
 		}
 		return head -> next;
 	}
