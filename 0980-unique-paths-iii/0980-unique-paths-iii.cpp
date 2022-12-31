2ad33d9c29eb2b31c4a41ae49b27f1050e285456
Yash Kumar <yashkumarjmp2002@gmail.com>
2022-12-31 09:49:23 +0530

    Time: 25 ms (18.02%), Space: 10.4 MB (16.65%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -8,29 +8,28 @@ public:
 		return i >= 0 && i < n && j >= 0 && j < m;
 	}
 
-	int dfs(int x, int y, int cnt, int &n, int &m, int &req, vector<vector<int>> &vis, vector<vector<int>> &grid) {
+	int dfs(int x, int y, int cnt, int &n, int &m, int &req, vector<vector<int>> &grid) {
 		if (grid[x][y] == -1) return 0;
 		else if (grid[x][y] == 2) {
 			if (cnt + 1 == req) return 1;
 			return 0;
 		}
-		vis[x][y] = 1; cnt++;
+		grid[x][y] = -1; cnt++;
 
 		int ans = 0;
 		for (auto it : movement) {
 			int currX = x + it[0],
 			    currY = y + it[1];
 			if (isValid(currX, currY, n, m)) {
-				if (!vis[currX][currY]) ans += dfs(currX, currY, cnt, n, m, req, vis, grid);
+				ans += dfs(currX, currY, cnt, n, m, req, grid);
 			}
 		}
-		vis[x][y] = 0; cnt--;
+		grid[x][y] = 0; cnt--;
 		return ans;
 	}
 
 	int uniquePathsIII(vector<vector<int>> &grid) {
 		int n = grid.size(), m = grid[0].size();
-		vector<vector<int>> vis(n, vector <int> (m));
 
 		int x, y, req = n * m;
 		for (int i = 0; i < n; i++) {
@@ -40,7 +39,7 @@ public:
 			}
 		}
 
-		int ans = dfs(x, y, 0, n, m, req, vis, grid);
+		int ans = dfs(x, y, 0, n, m, req, grid);
 
 		return ans;
 	}
