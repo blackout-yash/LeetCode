f022dcce7e5c69c7a19288b02b3c27ae91338a07
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-04-23 00:37:36 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -9,17 +9,13 @@ class Solution{
 		int add(int a, int b, int m = 1e9+7) {a %= m; b %=m; return (((a+b)%m)+m)%m;}
 
 	int func(int idx, int sum, int arr[], vector<vector<int>>&dp){
-	   //  if(idx == 0){
-    //         if(sum == 0 && arr[0] == 0){
-    //             return 2;
-    //         }
-    //         else if(sum == 0 || arr[0] == sum) return 1;
-    //         return 0;
-    //     }
-    if(idx == -1) {
-        if(sum) return 0;
-        return 1;
-    }
+	     if(idx == 0){
+            if(sum == 0 && arr[0] == 0){
+                return 2;
+            }
+            else if(sum == 0 || arr[0] == sum) return 1;
+            return 0;
+        }
 	    
 	    if(dp[idx][sum]!=-1) return dp[idx][sum];
 	    
