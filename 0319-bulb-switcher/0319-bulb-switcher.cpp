ae9f8b3147e326ce7da8f9e7ccc715fd9689b664
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-04-27 22:51:49 +0530

    Time: 3 ms (45.34%), Space: 5.9 MB (94.29%) - LeetHub

---
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        long low=0;
+        long high=n;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if((mid*mid)==n)
+                return (int)mid;
+            else if((mid*mid)<n)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return low-1;
+    }
+};
