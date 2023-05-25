ae45cdc115212e5aaf1f4b7898bc7ac3232c8306
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-05-25 17:27:01 +0530

    Time: 954 ms (21.39%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,3 +1,12 @@
 # SELECT e1.name AS Employee FROM Employee AS e1 CROSS JOIN Employee AS e2 ON e1.salary > e2.salary AND e1.managerId = e2.id;
 
-SELECT e1.name AS Employee FROM Employee AS e1 JOIN Employee AS e2 ON e1.salary > e2.salary AND e1.managerId = e2.id;
+# SELECT e1.name AS Employee FROM Employee AS e1 INNER JOIN Employee AS e2 ON e1.salary > e2.salary AND e1.managerId = e2.id;
+
+# SELECT e1.name AS Employee FROM Employee AS e1 JOIN Employee AS e2 ON e1.salary > e2.salary AND e1.managerId = e2.id;
+
+SELECT e1.name AS Employee 
+    FROM Employee AS e1,
+         Employee AS e2 
+         WHERE e1.salary > e2.salary 
+               AND 
+               e1.managerId = e2.id;
