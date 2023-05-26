1ce5759f60ef185e3ed0d0c6935d0e4328935d70
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-05-26 17:19:44 +0530

    Time: 770 ms (39.61%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,3 +1,5 @@
 # SELECT DISTINCT(email) as Email FROM Person GROUP BY email HAVING COUNT(email) > 1;
 
-SELECT email as Email FROM Person GROUP BY email HAVING COUNT(email) > 1;
+# SELECT email as Email FROM Person GROUP BY email HAVING COUNT(email) > 1;
+
+SELECT email FROM (SELECT email, COUNT(email) as cnt FROM Person GROUP BY email) AS Temp where cnt > 1;
