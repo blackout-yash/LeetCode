916d0047baa40f2f1eef533b2ca48be2ac66f416
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-06-25 23:21:26 +0530

    Time: 868 ms (8.29%), Space: 12.1 MB (19.34%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,6 +1,7 @@
-int mod = 1e9 + 7;
 class Solution {
 public:
+    int sum(int a, int b, int m = 1e9 + 7) {a %= m; b %= m; return (((a + b) % m) + m) % m;}
+    
     int countRoutes(vector <int> &locations, int start, int finish, int fuel) {
          int n = locations.size();
          vector <vector <int>> dp (n, vector <int> (fuel + 1));
@@ -11,13 +12,13 @@ public:
                 for(int j = 0; j < n; j++){
                     int diff = abs(locations[i] - locations[j]);
                     if(i != j && f >= diff)
-                    dp[j][f - diff] = (dp[j][f - diff] + dp[i][f]) % mod;
+                    dp[j][f - diff] = sum(dp[j][f - diff], dp[i][f]);
                 }
             }
         }
 
         int ans = 0;
-        for(int i = 0; i <= fuel; i++) ans = (ans + dp[finish][i])%mod;
+        for(int i = 0; i <= fuel; i++) ans = sum(ans, dp[finish][i]);
 
         return ans;   
     }
