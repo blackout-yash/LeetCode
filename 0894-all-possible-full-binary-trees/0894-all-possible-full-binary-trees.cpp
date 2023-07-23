976273a5917493ab19ced05e58317b0dff12577c
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-07-23 23:51:38 +0530

    Time: 116 ms (40.49%), Space: 35.1 MB (36.48%) - LeetHub

---
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<TreeNode*> allPossibleFBT(int n) {
+        if(n % 2 == 0) return {};
+        else if(n == 1) return {new TreeNode()};
+
+        vector <TreeNode*> ans;
+        for(int i = 1; i < n; i += 2){
+            vector <TreeNode*> left = allPossibleFBT(i);
+            vector <TreeNode*> right = allPossibleFBT(n - i - 1);
+            for(auto l : left){
+                for(auto r : right){
+                    TreeNode* root = new TreeNode();
+                    root -> left = l;
+                    root -> right = r;
+                    ans.push_back(root);
+                }
+            }
+        }
+        return ans;
+    }
+};
