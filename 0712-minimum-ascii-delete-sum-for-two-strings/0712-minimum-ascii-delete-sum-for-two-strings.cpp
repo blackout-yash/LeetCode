d54c0cf04dc1bc85613fd04c20a65f91b6cab7de
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-07-31 23:14:36 +0530

    Time: 38 ms (79.40%), Space: 14.9 MB (75.17%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     int minimumDeleteSum(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector <vector <int>> dp(n + 1, vector <int> (m + 1));
+        vector <int> dp(m + 1);
         for(int i = 0; i <= n; i++) {
+            vector <int> curr(m + 1);
             for(int j = 0; j <= m; j++) {
                 if(i && j) {
-                    int ans1 = dp[i - 1][j - 1];
+                    int ans1 = dp[j - 1];
                     if(s1[i - 1] != s2[j - 1]) ans1 += (s1[i - 1] + s2[j - 1]); 
                 
-                    int ans2 = s1[i - 1] + dp[i - 1][j];
-                    int ans3 = s2[j - 1] + dp[i][j - 1];
+                    int ans2 = s1[i - 1] + dp[j];
+                    int ans3 = s2[j - 1] + curr[j - 1];
                 
-                    dp[i][j] = min({ans1, ans2, ans3});
-                } else if(i) dp[i][j] = s1[i - 1] + dp[i - 1][j];
-                else if(j) dp[i][j] = s2[j - 1] + dp[i][j - 1];
+                    curr[j] = min({ans1, ans2, ans3});
+                } else if(i) curr[j] = s1[i - 1] + dp[j];
+                else if(j) curr[j] = s2[j - 1] + curr[j - 1];
             }
+            dp = curr;
         }
         
-        return dp[n][m];
+        return dp[m];
     }
 };
