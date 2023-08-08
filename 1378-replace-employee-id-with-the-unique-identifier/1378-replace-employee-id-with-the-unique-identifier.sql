71a27c3616b703496e91d06ec70ba88aa46524e7
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-08-09 01:14:55 +0530

    Time: 2840 ms (32.84%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
 
-select ei.unique_id, e.name from Employees as e left join EmployeeUNI as ei on e.id = ei.id;
+# select ei.unique_id, e.name from Employees as e left join EmployeeUNI as ei on e.id = ei.id;
+
+select ei.unique_id, e.name from Employees as e left join EmployeeUNI as ei using(id);
