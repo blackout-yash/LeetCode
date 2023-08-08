58a606b161de395c386d65f53b3b9261876d0c26
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-08-09 01:17:06 +0530

    Time: 4683 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier/1378-replace-employee-id-with-the-unique-identifier.sql
@@ -2,4 +2,7 @@
 
 # select ei.unique_id, e.name from Employees as e left join EmployeeUNI as ei on e.id = ei.id;
 
-select ei.unique_id, e.name from Employees as e left join EmployeeUNI as ei using(id);
+# select ei.unique_id, e.name from Employees as e left join EmployeeUNI as ei using(id);
+
+select ei.unique_id, e.name from EmployeeUNI ei right join Employees e using(id);
+ 
