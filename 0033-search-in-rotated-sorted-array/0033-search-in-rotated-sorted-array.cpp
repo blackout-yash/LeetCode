c16173baa54a57b31b6243c7218be3f56cf3574c
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-08-10 22:19:10 +0530

    Time: 3 ms (74.13%), Space: 11.2 MB (15.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,7 +5,8 @@ public:
         while(start <= end){
             int mid = start + (end-start)/2;
             if(nums[mid] == target) return mid;
-            else if(nums[start] <= nums[mid]){
+            else if(nums[start] == nums[mid]) start = mid + 1;
+            else if(nums[start] < nums[mid]){
                 if(nums[start] <= target && target <= nums[mid]) end = mid - 1;
                 else start = mid + 1;
             }else {
