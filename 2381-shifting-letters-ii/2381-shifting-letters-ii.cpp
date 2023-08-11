534ab1580ab97e01e3a1ab6dde5e1bcab7cb9316
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-08-11 15:14:20 +0530

    Time: 534 ms (10.17%), Space: 144.8 MB (5.08%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        map <int, int> posStart, negStart, posEnd, negEnd;
+        map <int, int> start, end;
         for(auto x: shifts) {
             int l = x[0], r = x[1];
             if(x[2]) {
-                posStart[l]++;
-                posEnd[r]--;
+                start[l]++;
+                end[r]--;
             } else {
-                negStart[l]--;
-                negEnd[r]++;
+                start[l]--;
+                end[r]++;
             }
         } 
         
         int n = s.size(), curr = 0;
         for(int i = 0; i < n; i++) {            
-            curr += (posStart[i] + negStart[i]);
+            curr += start[i];
             int temp = (((s[i] - 'a' + curr) % 26) + 26) % 26;
             s[i] = (char) 'a' + temp;
-            curr += (posEnd[i] + negEnd[i]);
+            curr += end[i];
         }
         return s;
     }
