1375eb107c063759e94c5921762067b920eda727
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-08-13 21:37:29 +0530

    Time: 122 ms (92.31%), Space: 83.7 MB (99.69%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     bool validPartition(vector <int> &nums) {
-        int n = nums.size();
-        vector <int> dp(n);
-        for(int i = 1; i < n; i++) {
-            bool ans1 = 0, ans2 = 0;
-            if(nums[i] == nums[i - 1]) {
-                if(i >= 2) ans1 = dp[i - 2];
-                else ans1 = true;
-            }
+        int n = nums.size(), ans1 = 1, ans2 = 1, ans3 = 1, ans4 = 1;
+        for(int i = 0; i < n; i++) {
+            bool curr1 = 0, curr2 = 0;
+            if(i && nums[i] == nums[i - 1]) curr1 = ans2;
             
-            if(i >= 2) {
-                if((nums[i] == nums[i - 1] && nums[i] == nums[i - 2]) ||
-                   (nums[i] - nums[i - 1] == 1 && nums[i - 1] - nums[i - 2] == 1)) {
-                    if(i >= 3) ans2 = dp[i - 3];
-                    else ans2 = true;
-                } 
-            }
+            if(i >= 2 && 
+              ((nums[i] == nums[i - 1] && nums[i] == nums[i - 2]) ||
+               (nums[i] - nums[i - 1] == 1 && nums[i - 1] - nums[i - 2] == 1))) curr2 = ans1;
             
-            dp[i] = (ans1 | ans2);
+            ans1 = ans2;
+            ans2 = ans3;
+            ans3 = (curr1 | curr2);
         }
-        return dp[n - 1];
+        return ans3;
     }
 };
