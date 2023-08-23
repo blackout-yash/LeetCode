9b40051a4bfe69c8e742d9b3ca89193390450d9b
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-08-23 23:18:39 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (46.07%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        vector <int> freq(26);
+        for(auto x: s) freq[x - 'a']++;
+        
+        int n = s.size(), max = n / 2 + (n % 2 != 0);
+        for(auto x: freq) {
+            if(x > max) return "";
+        }
+        
+        priority_queue <pair <int, char>> pq;
+        for(int i = 0; i < 26; i++) {
+            if(freq[i]) pq.push({freq[i], char('a' + i)});
+        }
+        
+        string ans;
+        while(pq.size()) {
+            int num = pq.top().first;
+            char ch = pq.top().second;
+            pq.pop();
+            if(ans.empty() || ans.back() != ch) {
+                num--;
+                ans.push_back(ch);
+            } else {
+                int num = pq.top().first;
+                char ch = pq.top().second;
+                pq.pop();
+                if(ans.back() != ch) {
+                    ans.push_back(ch);
+                    if(num > 1) pq.push({num - 1, ch});
+                } else return "";
+            }
+            if(num) pq.push({num, ch});
+        }
+        return ans;
+    }
+};
