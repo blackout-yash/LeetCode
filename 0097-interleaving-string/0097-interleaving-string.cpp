73471f654cdaeb65228b9ba8a9da4cf7a79abc10
Yash Kumar <yashkumarjmp2002@gmail.com>
2023-08-26 00:54:51 +0530

    Time: 4 ms (70.98%), Space: 7.1 MB (58.49%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    bool isInterleave(int i, int j, int k, int n, int m, string &s1, string &s2, string &s3, vector <vector <int>> &dp) {
-        if(i == n && j == m) return true;
-        else if(dp[i][j] != -1) return dp[i][j];
+    bool isInterleave(int n, int m, int k, string &s1, string &s2, string &s3, vector <vector <int>> &dp) {
+        if(n == -1 && m == -1) return true;
+        else if(dp[n + 1][m + 1] != -1) return dp[n + 1][m + 1];
         
         bool ans1 = false;
-        if(i != n && s1[i] == s3[k]) ans1 = isInterleave(i + 1, j, k + 1, n, m, s1, s2, s3, dp);
+        if(n != -1 && s1[n] == s3[k]) ans1 = isInterleave(n - 1, m, k - 1, s1, s2, s3, dp);
         
         bool ans2 = false;
-        if(j != m && s2[j] == s3[k]) ans2 = isInterleave(i, j + 1, k + 1, n, m, s1, s2, s3, dp);
+        if(m != -1 && s2[m] == s3[k]) ans2 = isInterleave(n, m - 1, k - 1, s1, s2, s3, dp);
         
-        return dp[i][j] = (ans1 | ans2);
+        return dp[n + 1][m + 1] = (ans1 | ans2);
     }
     
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.size(), m = s2.size();
-        if(n + m != s3.size()) return false;
+        int n = s1.size(), m = s2.size(), k = s3.size();
+        if(n + m != k) return false;
         vector <vector <int>> dp(n + 1, vector <int> (m + 1, -1));
-        return isInterleave(0, 0, 0, n, m, s1, s2, s3, dp);
+        return isInterleave(n - 1, m - 1, k - 1, s1, s2, s3, dp);
     }
 };
