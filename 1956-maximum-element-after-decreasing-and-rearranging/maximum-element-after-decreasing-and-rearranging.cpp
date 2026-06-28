d05c2a9d86fcf5b79beb29dfdb387dc329077ea0
Yash Kumar <yashkumarjmp2002@gmail.com>
2026-06-28 23:28:56 +0530

    Time: 4 ms (88.98%) | Memory: 55.2 MB (36.72%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -4,8 +4,7 @@ public:
         int ans = 0, curr = 0;
         sort(begin(arr), end(arr));
         for(auto x: arr) {
-            if(x <= curr) {}
-            else {
+            if(x > curr) {
                 curr++;
                 ans = curr;
             }
